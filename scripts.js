5c2b0adac88a96ddb3ca3a382d50ca73168cd811
unknown <ynunciraf@gmail.com>
2024-08-26 11:52:48 -0500

    cambios medios

--- scripts.js
+++ scripts.js
@@ -1,15 +1,44 @@
-// script.js
+// login form
 document.addEventListener("DOMContentLoaded", function() {
     var loginForm = document.getElementById("loginForm");
-    loginForm.addEventListener("submit", function(event) {
-        event.preventDefault();
-        var username = document.getElementById("username").value;
-        var password = document.getElementById("password").value;
+    if (loginForm){
+        loginForm.addEventListener("submit", function(event){
+            event.preventDefault();
+            var username = document.getElementById("username").value;
+            var password = document.getElementById("password").value;
 
-        if (username === "admin" && password === "1234") {
-            window.location.href = "dashboard.html";
-        } else {
-            alert("Usuario o contraseña incorrectos");
-        }
+            if(usuario == "admin" && password == "1234"){
+                window.location.href = "dashboard.html";
+            } else {
+                alert("Usuario o contraseña incorrectos");
+            }
+        });
+    }
+
+    var registerForm = document.getElementById("registerForm");
+    if(registerForm){
+        registerForm.addEventListener("submit", function(event){
+            event.preventDefault();
+            var newUser = document.getElementById("newUser").value;
+            var newPassword = document.getElementById("newpassword").value;
+            
+            alert("Usuario registrado: " + newUser);
+            window.location.href = "index.html";
+        });
+    }
+    var linkEstudiantes = document.getElementById("linkEstudiantes");
+    var contenedorEstudiantes = document.getElementById("contenedorEstudiantes");
+
+    linkEstudiantes.addEventListener("click", function(event) {
+        event.preventDefault();
+        contenedorEstudiantes.classList.add("active");
+        contenedorAsignaturas.classList.remove("active");
     });
+
+    LinkAsignaturas.addEventListener("click", function(event) {
+        event.preventDefault();
+        contenedorAsignaturas.classList.add("active");
+        contenedorEstudiantes.classList.remove("active");
+    });
+
 });
